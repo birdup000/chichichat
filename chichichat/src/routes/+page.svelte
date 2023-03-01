a4f1f53de174ee5fe9165f93faf9f6519a621e96
pydev <84549156+pydevMan@users.noreply.github.com>
2023-03-01 13:15:15 -0600

    added home and about sectoins

--- chichichat/src/routes/+page.svelte
+++ chichichat/src/routes/+page.svelte
@@ -1,2 +1,88 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<html lang="en">
+
+<head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.0/jquery.min.js"></script>
+    <title>Home</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="bees.ico" type="image/x-icon">
+    <style>
+        body {
+  shape-margin: 5% black;
+  background-color: black;
+  font-style: normal;
+}
+h1 {
+  color: #FFFF;
+}
+h2 {
+  color: #FFFF;
+}
+
+p {
+  color: #FFFF;
+}
+
+/* Add a black background color to the top navigation */
+.topnav {
+  background-color: #333;
+  overflow: hidden;
+  text-align: center;
+  border-radius: 5px;
+
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+  -webkit-box-shadow: 5px 5px 5px rgba(68, 196, 132, 0.712);
+          box-shadow: 5px 5px 5px rgba(68, 196, 132, 0.712);
+  width: 80px;
+  display: inline-block;
+  border-radius: 5px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+  border-radius: 5px;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+  background-color: #414141;
+  color: white;
+}
+
+.content {
+  color: #4b4949;
+}
+    </style>
+
+</head>
+
+<body>
+    <div class="topnav">
+        <a class="active" href="/">Home</a>
+        <a href="/contact">Contact</a>
+        <a href="/about">About</a>
+        <a href="/dashboard">Dashboard</a>
+    </div>
+    <div class="content">
+        <p style="margin-left: 15%; font-size: 200%;"><b>Welcome to</b></p>
+        <h1 style="margin-left: 25%; font-size: 60px;">Sassychat Peer and Private Chatting Platform!!</h1>
+
+    </div>
+
+    <script>
+
+    </script>
+
+</body>
+
+</html>
--- chichichat/src/routes/about/+page.svelte
+++ chichichat/src/routes/about/+page.svelte
@@ -1,2 +1,76 @@
-<h1>About this site</h1>
-<p>TODO...</p>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link rel="shortcut icon" href="bees.ico" type="image/x-icon">
+    <style>body {
+        shape-margin: 5% black;
+        background-color: black;
+        font-style: normal;
+      }
+      h1 {
+        color: #FFFF;
+      }
+      h2 {
+        color: #FFFF;
+      }
+      
+      p {
+        color: #FFFF;
+      }
+      
+      /* Add a black background color to the top navigation */
+      .topnav {
+        background-color: #333;
+        overflow: hidden;
+        text-align: center;
+        border-radius: 5px;
+      
+      }
+      
+      /* Style the links inside the navigation bar */
+      .topnav a {
+        color: #f2f2f2;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+        font-size: 17px;
+        -webkit-box-shadow: 5px 5px 5px rgba(68, 196, 132, 0.712);
+                box-shadow: 5px 5px 5px rgba(68, 196, 132, 0.712);
+        width: 80px;
+        display: inline-block;
+        border-radius: 5px;
+      }
+      
+      /* Change the color of links on hover */
+      .topnav a:hover {
+        background-color: #ddd;
+        color: black;
+        border-radius: 5px;
+      }
+      
+      /* Add a color to the active/current link */
+      .topnav a.active {
+        background-color: #414141;
+        color: white;
+      }
+      
+      .content {
+        color: #4b4949;
+      }
+      
+      
+      
+      </style>
+</head>
+<body>
+    <div class="topnav">
+        <a href="/">Home</a>
+        <a href="/contact">Contact</a>
+        <a class="active" href="/about">About</a>
+        <a href="/dashboard">Dashboard</a>
+      </div>
+      <h2>About Sassy Chat</h2>
+       <p><b>This site is coded by currently two awesome people trying to make an peer and privacy based chat service that is great.</b></p>
+</body>
+</html>
